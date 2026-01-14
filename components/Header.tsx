030eabae9a0ba94160d917fadc1aaba89327eb74
hangel6100-spec <h.angel.6100@gmail.com>
2026-01-14 04:26:18 -0500

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,17 @@
 'use client'
 
+import React from 'react'
 import { useState, useEffect, useRef, useCallback } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 
+// Type definitions
+interface DropdownItem {
+  label: string
+  href: string
+}
+import Image from 'next/image'
+import Link from 'next/link'
 // Type definitions
 interface DropdownItem {
   label: string
